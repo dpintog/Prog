04a555f2982ab574e405e21c147dcad209c55719
Diana Milena Pinto Gonzalez <dpintog@darkstar.org>
2019-09-06 08:14:39 -0500

    este es mi primercommit

---
+++ 04-09-intro/holamundo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>// uso de la biblioteca
+int main (void)
+{
+  double x = 0.0;
+  std::cout<< "escribe": <<
+  std::cout <<"Hola mundo"<< std::endl;
+  //comentario
+  return 0;
+}
