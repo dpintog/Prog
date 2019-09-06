edc00e45e355a27e0af23531664967359d7b0e7f
Diana Milena Pinto Gonzalez <dpintog@darkstar.org>
2019-09-06 08:55:55 -0500

    leeeeer

---
+++ 04-09-intro/leer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>// uso de la biblioteca
+int main (void)
+{
+  double x = 0.0;
+  std::cout<< "escribe:" <<
+    std:: cin>> x;
+  std::cout <<"He leido"<< std::endl;
+  //comentario
+  return 0;
+}
